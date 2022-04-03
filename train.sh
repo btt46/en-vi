33cd754af95850707e6973272a0450fe8023b921
btt46 <tuanthanhbui46@gmail.com>
2022-04-04 01:48:21 +0900

    fix bugs

--- train.sh
+++ train.sh
@@ -9,7 +9,7 @@ DATASET=$PWD/data
 BIN_DATA=$DATASET/tmp/bin-data
 
 CUDA_VISIBLE_DEVICES=$GPUS fairseq-train $BIN_DATA -s ${src} -t ${tgt} \
-		            -arch transformer_iwslt_de_en --share-decoder-input-output-embed \
+		            --arch transformer_iwslt_de_en --share-decoder-input-output-embed \
                     --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
                     --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
                     --dropout 0.3 --weight-decay 0.0001 \
