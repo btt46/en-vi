7179dbadeee8e45a5e51a58532e97a34a353c7ae
btt46 <tuanthanhbui46@gmail.com>
2022-04-04 01:44:03 +0900

    update models

--- train.sh
+++ train.sh
@@ -3,32 +3,25 @@ tgt=vi
 
 GPUS=$1
 MODEL_NAME=$2
-EPOCHS=$3
 
 EXPDIR=$PWD
 DATASET=$PWD/data
 BIN_DATA=$DATASET/tmp/bin-data
 
 CUDA_VISIBLE_DEVICES=$GPUS fairseq-train $BIN_DATA -s ${SRC} -t ${TGT} \
-		            --log-interval 100 \
-					--log-format json \
-					--max-epoch ${EPOCHS} \
-		    		--optimizer adam --lr 0.0001 \
-					--clip-norm 0.0 \
-					--max-tokens 4000 \
-					--no-progress-bar \
-					--log-interval 100 \
-					--min-lr '1e-09' \
-					--weight-decay 0.0001 \
-					--criterion label_smoothed_cross_entropy \
-					--label-smoothing 0.1 \
-					--lr-scheduler inverse_sqrt \
-					--warmup-updates 4000 \
-					--warmup-init-lr '1e-08' \
-					--adam-betas '(0.9, 0.98)' \
-					--arch transformer_iwslt_de_en \
-					--dropout 0.1 \
-					--attention-dropout 0.1 \
+		            -arch transformer_iwslt_de_en --share-decoder-input-output-embed \
+                    --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
+                    --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
+                    --dropout 0.3 --weight-decay 0.0001 \
+                    --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
+                    --max-tokens 4096 \
+                    --patience 5 \
+                    --eval-bleu \
+                    --eval-bleu-args '{"beam": 5, "max_len_a": 1.2, "max_len_b": 10}' \
+                    --eval-bleu-detok moses \
+                    --eval-bleu-remove-bpe \
+                    --eval-bleu-print-samples \
+                    --best-checkpoint-metric bleu --maximize-best-checkpoint-metric \
 					--save-dir $EXPDIR/models/$MODEL_NAME \
 					2>&1 | tee $LOG/${MODEL_NAME}
 
