c5f6d929f35b878ae7d35d2d48e969a73f713e30
btt46 <tuanthanhbui46@gmail.com>
2022-04-04 10:07:28 +0900

    fix bugs

--- eval.sh
+++ eval.sh
@@ -29,7 +29,7 @@ cat ${PWD}/results/${MODEL_NAME}/valid_trans.${tgt} | sed -r 's/(@@ )|(@@ ?$)//g
 $DETRUECASER < ${PWD}/results/${MODEL_NAME}/valid_rmvbpe.${tgt} > ${PWD}/results/${MODEL_NAME}/valid_detruecase.${tgt}
 
 # detokenize
-python3.6 $PWD/postprocess/ ${PWD}/results/${MODEL_NAME}/valid_detruecase.${tgt} ${PWD}/results/${MODEL_NAME}/valid.${tgt}
+python3.6 $PWD/postprocess/detokenize.py ${PWD}/results/${MODEL_NAME}/valid_detruecase.${tgt} ${PWD}/results/${MODEL_NAME}/valid.${tgt}
 
 echo "VALID" >> ${PWD}/results/${MODEL_NAME}/valid_result.txt
 env LC_ALL=en_US.UTF-8 perl $PWD/multi-bleu.pl $PWD/data/valid.${tgt} < ${PWD}/results/${MODEL_NAME}/valid.${tgt} >> ${PWD}/results/${MODEL_NAME}/valid_result.txt
@@ -48,7 +48,7 @@ cat ${PWD}/results/${MODEL_NAME}/test_trans.${tgt} | sed -r 's/(@@ )|(@@ ?$)//g'
 $DETRUECASER < ${PWD}/results/${MODEL_NAME}/test_rmvbpe.${tgt} > ${PWD}/results/${MODEL_NAME}/test_detruecase.${tgt}
 
 # detokenize
-python3.6 $PWD/postprocess/ ${PWD}/results/${MODEL_NAME}/test_detruecase.${tgt} ${PWD}/results/${MODEL_NAME}/test.${tgt}
+python3.6 $PWD/postprocess/detokenize.py ${PWD}/results/${MODEL_NAME}/test_detruecase.${tgt} ${PWD}/results/${MODEL_NAME}/test.${tgt}
 
 echo "TEST" >> ${PWD}/results/${MODEL_NAME}/test_result.txt
 env LC_ALL=en_US.UTF-8 perl $PWD/multi-bleu.pl $PWD/data/test.${tgt} < ${PWD}/results/${MODEL_NAME}/test.${tgt} >> ${PWD}/results/${MODEL_NAME}/test_result.txt
