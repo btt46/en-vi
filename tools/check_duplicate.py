22e5a780de50e96ff26ecfcab633e1be592adadb
btt46 <tuanthanhbui46@gmail.com>
2022-04-07 15:06:13 +0900

    add tools

---
+++ tools/check_duplicate.py
@@ -0,0 +1,32 @@
+import re
+from weakref import ref
+import sentencepiece 
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('-i','--input_file', type=str, required=True, dest='input', help='Input file name')
+    parser.add_argument('-r','--ref_file', type=str, required=True, dest='ref', help='Reference file name')
+    
+    args = parser.parse_args()
+    input_contents = []
+    ref_contents = []
+
+    with open(args.input, 'r', encoding='utf-8') as fp:
+        input_contents = fp.readlines()
+        fp.close()
+
+    with open(args.ref, 'r', encoding='utf-8') as fp:
+        ref_contents = fp.readlines()
+        fp.close()
+
+    score = []
+    for line in input_contents:
+        if line in ref_contents:
+            score += 1
+
+    print(f"Score: {score}")
+
+if __name__ == '__main__':
+    main()
+
